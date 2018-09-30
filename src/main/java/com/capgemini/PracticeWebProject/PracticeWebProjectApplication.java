4c7d9cb8e128fb4cd3a29269d5cb28410180f643
Tim Neefjes <tim.neefjes@capgemini.com>
2018-09-30 18:18:49 +0200

    First commit in my PracticeWebProjectApplication repository

---
+++ src/main/java/com/capgemini/PracticeWebProject/PracticeWebProjectApplication.java
@@ -0,0 +1,12 @@
+package com.capgemini.PracticeWebProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeWebProjectApplication.class, args);
+	}
+}
